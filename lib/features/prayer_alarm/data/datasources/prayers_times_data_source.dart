7b94f35cb5afe05e8ec0d5bf3983945b2d3884cb
ahmednagy <ahmednagyhero25@gmail.com>
2024-10-07 11:00:57 +0300

    switch cache in the repository

--- lib/features/prayer_alarm/data/datasources/prayers_times_data_source.dart
+++ lib/features/prayer_alarm/data/datasources/prayers_times_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:muslim_alarm/core/network/api_client.dart';
+import 'package:muslim_alarm/core/network/dio_api_client.dart';
 import 'package:muslim_alarm/core/network/endpoints.dart';
 import 'package:muslim_alarm/features/prayer_alarm/data/models/timings_model.dart';
 import 'package:muslim_alarm/features/prayer_alarm/data/models/timings_response_data.dart';
@@ -8,6 +10,9 @@ abstract class PrayersTimesDataSource {
 }
 
 class PrayersTimesDataSourceImpl implements PrayersTimesDataSource {
+  static final provider = Provider((ref) => PrayersTimesDataSourceImpl(
+        apiClient: ref.watch(DioApiClient.provider),
+      ));
   final ApiClient apiClient;
 
   PrayersTimesDataSourceImpl({required this.apiClient});
--- lib/features/prayer_alarm/data/repositories/prayer_alarm_repository.dart
+++ lib/features/prayer_alarm/data/repositories/prayer_alarm_repository.dart
@@ -2,24 +2,33 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:muslim_alarm/core/failures/failure.dart';
 import 'package:muslim_alarm/core/failures/general_failure.dart';
-import 'package:muslim_alarm/core/network/dio_api_client.dart';
+import 'package:muslim_alarm/features/prayer_alarm/data/datasources/prayers_times_cache_data_source.dart';
 import 'package:muslim_alarm/features/prayer_alarm/data/datasources/prayers_times_data_source.dart';
 import 'package:muslim_alarm/features/prayer_alarm/data/mappers/timings.dart';
 import 'package:muslim_alarm/features/prayer_alarm/domain/entities/timings.dart';
 import 'package:muslim_alarm/features/prayer_alarm/domain/repositories/prayer_alarm_repository.dart';
 
 class PrayerAlarmRepositoryImpl implements PrayerAlarmRepository {
-  static final provider =
-      Provider<PrayerAlarmRepository>((ref) => PrayerAlarmRepositoryImpl(
-            timingsDataSource: PrayersTimesDataSourceImpl(
-                apiClient: ref.watch(DioApiClient.provider)),
-          ));
+  static final provider = Provider<PrayerAlarmRepository>((ref) {
+    final cacheDataSource = ref.watch(PrayersTimesCacheDataSourceImpl.provider);
+    final timingsDataSource = ref.watch(PrayersTimesDataSourceImpl.provider);
+    return PrayerAlarmRepositoryImpl(
+        timingsDataSource: timingsDataSource, cacheDataSource: cacheDataSource);
+  });
   final PrayersTimesDataSource timingsDataSource;
-  PrayerAlarmRepositoryImpl({required this.timingsDataSource});
+  final PrayersTimesCacheDataSource cacheDataSource;
+  PrayerAlarmRepositoryImpl(
+      {required this.timingsDataSource, required this.cacheDataSource});
   @override
   Future<Either<Failure, Timings>> getPrayerTimes(
       {required double lat, required double long}) async {
     try {
+      final cacheData = cacheDataSource.getPrayerTimes();
+
+      if (cacheData != null) {
+        return Right(cacheData.toEntity);
+      }
+
       final data = await timingsDataSource.getTimings(lat: lat, long: long);
       return Right(data.toEntity);
     } catch (e) {
